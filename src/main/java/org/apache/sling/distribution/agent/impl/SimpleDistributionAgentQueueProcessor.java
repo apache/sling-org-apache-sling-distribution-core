3baa66137d8a72089fd6754a4db36e2e0f14b896
Ashish Chopra <ashishc@adobe.com>
2020-01-30 12:08:45 +0530

    SLING-9030 incorporating review comments [0]
    [0] https://github.com/apache/sling-org-apache-sling-distribution-core/pull/34#issuecomment-579241722

--- src/main/java/org/apache/sling/distribution/agent/impl/SimpleDistributionAgentQueueProcessor.java
+++ src/main/java/org/apache/sling/distribution/agent/impl/SimpleDistributionAgentQueueProcessor.java
@@ -148,11 +148,8 @@ class SimpleDistributionAgentQueueProcessor implements DistributionQueueProcesso
                     distributionLog.error("[{}] PACKAGE-FAIL {}: could not deliver {}, {}", queueName, requestId, distributionPackage.getId(), e.getMessage());
                     distributionLog.debug("could not deliver package {}", distributionPackage.getId(), e);
 
-                    if (log.isDebugEnabled()) {
-                        log.error("could not deliver package {}, will retry", distributionPackage.getId(), e);
-                    } else {
-                        log.warn("could not deliver package {} due to {}, will retry", distributionPackage.getId(), e.getMessage());
-                    }
+                    log.warn("could not deliver package {} due to {}, will retry", distributionPackage.getId(), e.getMessage());
+                    log.debug("Stacktrace: ", e);
 
                 } catch (Throwable e) {
                     distributionLog.error("[{}] PACKAGE-FAIL {}: could not deliver package {} {}", queueName, requestId, distributionPackage.getId(), e.getMessage(), e);
